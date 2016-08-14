b373cc37c2ce588863123ed3bee32cc35e20228d
Rnato Neto <rnato.neto@hotmail.com>
2016-08-14 01:07:21 -0300

    Project started with database ready

---
+++ pre-pesagem/Home.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace pre_pesagem
+{
+    partial class Home
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.produtosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarProdutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarProdutoToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listarProdutosToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gerarRelatórioDeSaídaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.relatóriosDeSaídaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.préPesagemToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.abrirOSToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oSAbertaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oSFechadaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.receitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cadastrarReceitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editarReceitaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listarReceitasToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.produtosToolStripMenuItem,
+            this.préPesagemToolStripMenuItem,
+            this.receitasToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(813, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Image = global::pre_pesagem.Properties.Resources._12039460_1673532516221083_277882707403000927_n;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 24);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(813, 533);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // produtosToolStripMenuItem
+            // 
+            this.produtosToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarProdutoToolStripMenuItem,
+            this.editarProdutoToolStripMenuItem,
+            this.listarProdutosToolStripMenuItem,
+            this.gerarRelatórioDeSaídaToolStripMenuItem,
+            this.relatóriosDeSaídaToolStripMenuItem});
+            this.produtosToolStripMenuItem.Name = "produtosToolStripMenuItem";
+            this.produtosToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.produtosToolStripMenuItem.Text = "Produtos";
+            // 
+            // cadastrarProdutoToolStripMenuItem
+            // 
+            this.cadastrarProdutoToolStripMenuItem.Name = "cadastrarProdutoToolStripMenuItem";
+            this.cadastrarProdutoToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.cadastrarProdutoToolStripMenuItem.Text = "Cadastrar produto";
+            // 
+            // editarProdutoToolStripMenuItem
+            // 
+            this.editarProdutoToolStripMenuItem.Name = "editarProdutoToolStripMenuItem";
+            this.editarProdutoToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.editarProdutoToolStripMenuItem.Text = "Editar produto";
+            // 
+            // listarProdutosToolStripMenuItem
+            // 
+            this.listarProdutosToolStripMenuItem.Name = "listarProdutosToolStripMenuItem";
+            this.listarProdutosToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.listarProdutosToolStripMenuItem.Text = "Listar produtos";
+            // 
+            // gerarRelatórioDeSaídaToolStripMenuItem
+            // 
+            this.gerarRelatórioDeSaídaToolStripMenuItem.Name = "gerarRelatórioDeSaídaToolStripMenuItem";
+            this.gerarRelatórioDeSaídaToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.gerarRelatórioDeSaídaToolStripMenuItem.Text = "Gerar relatório de saída";
+            // 
+            // relatóriosDeSaídaToolStripMenuItem
+            // 
+            this.relatóriosDeSaídaToolStripMenuItem.Name = "relatóriosDeSaídaToolStripMenuItem";
+            this.relatóriosDeSaídaToolStripMenuItem.Size = new System.Drawing.Size(195, 22);
+            this.relatóriosDeSaídaToolStripMenuItem.Text = "Relatórios de saída";
+            // 
+            // préPesagemToolStripMenuItem
+            // 
+            this.préPesagemToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.abrirOSToolStripMenuItem,
+            this.oSAbertaToolStripMenuItem,
+            this.oSFechadaToolStripMenuItem});
+            this.préPesagemToolStripMenuItem.Name = "préPesagemToolStripMenuItem";
+            this.préPesagemToolStripMenuItem.Size = new System.Drawing.Size(89, 20);
+            this.préPesagemToolStripMenuItem.Text = "Pré-Pesagem";
+            // 
+            // abrirOSToolStripMenuItem
+            // 
+            this.abrirOSToolStripMenuItem.Name = "abrirOSToolStripMenuItem";
+            this.abrirOSToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.abrirOSToolStripMenuItem.Text = "Abrir OS";
+            // 
+            // oSAbertaToolStripMenuItem
+            // 
+            this.oSAbertaToolStripMenuItem.Name = "oSAbertaToolStripMenuItem";
+            this.oSAbertaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oSAbertaToolStripMenuItem.Text = "OS Aberta";
+            // 
+            // oSFechadaToolStripMenuItem
+            // 
+            this.oSFechadaToolStripMenuItem.Name = "oSFechadaToolStripMenuItem";
+            this.oSFechadaToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.oSFechadaToolStripMenuItem.Text = "OS Fechada";
+            // 
+            // receitasToolStripMenuItem
+            // 
+            this.receitasToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cadastrarReceitasToolStripMenuItem,
+            this.editarReceitaToolStripMenuItem,
+            this.listarReceitasToolStripMenuItem});
+            this.receitasToolStripMenuItem.Name = "receitasToolStripMenuItem";
+            this.receitasToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.receitasToolStripMenuItem.Text = "Receitas";
+            // 
+            // cadastrarReceitasToolStripMenuItem
+            // 
+            this.cadastrarReceitasToolStripMenuItem.Name = "cadastrarReceitasToolStripMenuItem";
+            this.cadastrarReceitasToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.cadastrarReceitasToolStripMenuItem.Text = "Cadastrar receitas";
+            // 
+            // editarReceitaToolStripMenuItem
+            // 
+            this.editarReceitaToolStripMenuItem.Name = "editarReceitaToolStripMenuItem";
+            this.editarReceitaToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.editarReceitaToolStripMenuItem.Text = "Editar receita";
+            // 
+            // listarReceitasToolStripMenuItem
+            // 
+            this.listarReceitasToolStripMenuItem.Name = "listarReceitasToolStripMenuItem";
+            this.listarReceitasToolStripMenuItem.Size = new System.Drawing.Size(167, 22);
+            this.listarReceitasToolStripMenuItem.Text = "Listar receitas";
+            // 
+            // Home
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(813, 557);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Home";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Pré-Pesagem";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem produtosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarProdutoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarProdutoToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listarProdutosToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gerarRelatórioDeSaídaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem relatóriosDeSaídaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem préPesagemToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem abrirOSToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oSAbertaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oSFechadaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem receitasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cadastrarReceitasToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editarReceitaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listarReceitasToolStripMenuItem;
+    }
+}
+
